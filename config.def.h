268c882225df4b7b08af8dd7280280e6393837f2
isyuck <kaazification@gmail.com>
2020-08-05 06:45:14 +0100

    reduce number of tags; remove bar toggle bind

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 0;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -39,7 +39,7 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5"};
 
 static const Rule rules[] = {
 	/*	WM_CLASS(STRING) = instance, class
@@ -83,7 +83,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xst", "-e", "/usr/bin/zsh", NULL };
-static const char *roficmd[]  = { "rofi", "-show", "run", "-display-run", ">", "-font", "IBM Plex Mono Bold 12", "-theme", "~/.config/isaac.rasi", NULL };
+static const char *roficmd[]  = { "rofi", "-show", "run", "-display-run", ">", "-font", "scientifica 8", "-theme", "~/.config/isaac.rasi", NULL };
 
 #include <X11/XF86keysym.h>
 static Key keys[] = {
@@ -124,7 +124,6 @@ static Key keys[] = {
  	{ MODKEY|ShiftMask,		    XK_Return,	          togglescratch,    {.ui = 0} },
 	{ MODKEY,		            XK_z,		          incrgaps,	        {.i = +8 } },
 	{ MODKEY,		            XK_x,		          incrgaps,	        {.i = -8 } },
-	{ MODKEY,		            XK_b,		          togglebar,	    {0} },
 	{ MODKEY,			        XK_Left,	          focusmon,	        {.i = -1 } },
 	{ MODKEY|ShiftMask,		    XK_Left,	          tagmon,		    {.i = -1 } },
 	{ MODKEY,			        XK_Right,	          focusmon,	        {.i = +1 } },
